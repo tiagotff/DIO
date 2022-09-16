67a38e510ab001f6208bf6055999a038071ae11d
Tiago de Freitas Faustino <t.faustino91@gmail.com>
2022-09-16 15:22:04 -0300

    Modulo 2 concluido

---
+++ Ciencia_de_Dados_Unimed_BH/Modulo_2 _Python_para_Cientistas_de_Dados/exercicios_aulas/string3.py
@@ -0,0 +1,9 @@
+nome = "Guilherme Arthur de Carvalo"
+
+print(nome[0])
+print(nome[9])
+print(nome[10:])
+print(nome[10:16])
+print(nome[10:16:2])
+print(nome[:])
+print(nome[::-1])
---
+++ Ciencia_de_Dados_Unimed_BH/Modulo_2 _Python_para_Cientistas_de_Dados/exercicios_aulas/variaveis_constantes.py
@@ -0,0 +1,17 @@
+nome = 'Guilherme'
+
+idade = 28
+
+nome = 'Giovana'
+
+print(nome, idade)
+
+limite_saque_diario = 1000
+
+saque_diario = 500
+
+BRAZILIAN_STATES = ['sp', 'rj', 'ba']
+
+print(BRAZILIAN_STATES)
+
+
