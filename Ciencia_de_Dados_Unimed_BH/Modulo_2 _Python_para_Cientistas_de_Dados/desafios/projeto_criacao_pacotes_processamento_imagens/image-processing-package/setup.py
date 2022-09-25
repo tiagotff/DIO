c1ba42387d73bbeaad1a2551bad4996182947008
Tiago de Freitas Faustino <53586684+tiagotff@users.noreply.github.com>
2022-09-25 12:20:57 -0300

    Update setup.py

--- Ciencia_de_Dados_Unimed_BH/Modulo_2 _Python_para_Cientistas_de_Dados/desafios/projeto_criacao_pacotes_processamento_imagens/image-processing-package/setup.py
+++ Ciencia_de_Dados_Unimed_BH/Modulo_2 _Python_para_Cientistas_de_Dados/desafios/projeto_criacao_pacotes_processamento_imagens/image-processing-package/setup.py
@@ -14,7 +14,7 @@ setup(
     description="Test 1 version - Image processing. This project belongs to Karina Tiemi Kato, Tech Lead, Machine Learning Engineer, Data Scientist Specialist at Take. This package is a demo for simulation of upload on the Test Pypi website, and it's from one of the classes from Tech Generation Unimed - BH - Data Science Bootcamp. E-mail:karinatkato@gmail.com",
     long_description=page_description,
     long_description_content_type="text/markdown",
-    url="my_github_repository_project_link"
+    url="https://github.com/tiagotff/DIO/tree/main/Ciencia_de_Dados_Unimed_BH/Modulo_2%20_Python_para_Cientistas_de_Dados/desafios/projeto_criacao_pacotes_processamento_imagens/image-processing-package",
     packages=find_packages(),
     install_requires=requirements,
     python_requires='>=3.8',)
