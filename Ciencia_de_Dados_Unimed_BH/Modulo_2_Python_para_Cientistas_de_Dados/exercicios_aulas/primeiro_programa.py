897e40fd45f15ef35fc94c79539720a457a75d38
Tiago de Freitas Faustino <53586684+tiagotff@users.noreply.github.com>
2022-09-25 19:52:44 -0300

    Delete Ciencia_de_Dados_Unimed_BH directory

--- Ciencia_de_Dados_Unimed_BH/Modulo_2_Python_para_Cientistas_de_Dados/exercicios_aulas/primeiro_programa.py
+++
@@ -1,2 +0,0 @@
-print("Hello, world!")
-print("Oi, seja bem-vindo ao mundo de Python!")
--- Ciencia_de_Dados_Unimed_BH/Modulo_2_Python_para_Cientistas_de_Dados/exercicios_aulas/string3.py
+++
@@ -1,9 +0,0 @@
-nome = "Guilherme Arthur de Carvalo"
-
-print(nome[0])
-print(nome[9])
-print(nome[10:])
-print(nome[10:16])
-print(nome[10:16:2])
-print(nome[:])
-print(nome[::-1])
